9391a9a8e91c8307d7b7a5c751275fc5aa8c00a6
Alex Gherghisan <alexghr@users.noreply.github.com>
2021-07-11 09:10:39 +0100

    refactor max refs as an option

--- src/lib/dag/naive.ts
+++ src/lib/dag/naive.ts
@@ -1,15 +1,19 @@
 import { Readable } from "stream";
 import { makeDagNode, DagNode } from "./node";
 
+const DEFAULT_MAX_REFS = 4;
 
-// each node can have up to this many refs
-export const MAX_REFS = 4;
+type Options = {
+  /** each node can have up to this many refs */
+  maxRefs?: number
+};
 
 /**
  * creates a dag from the chunked data in the stream
  */
-export async function createLeftHeavyDag(stream: Readable): Promise<DagNode | null> {
+export async function createLeftHeavyDag(stream: Readable, opts: Options = {}): Promise<DagNode | null> {
   let last: DagNode | null = null;
+  const { maxRefs = DEFAULT_MAX_REFS } = opts;
 
   return new Promise((res, rej) => {
     stream
@@ -21,7 +25,7 @@ export async function createLeftHeavyDag(stream: Readable): Promise<DagNode | nu
           const kid = makeDagNode(chunk);
           const parent = makeDagNode(null, [last.id, kid.id]);
           last = parent;
-        } else if ((last.refs?.length ?? 0) >= MAX_REFS) {
+        } else if ((last.refs?.length ?? 0) >= maxRefs) {
           const kid = makeDagNode(chunk);
           const parent = makeDagNode(null, [last.id, kid.id]);
           last = parent;
